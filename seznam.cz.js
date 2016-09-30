daf4105051e3bce67daed5a9d827eef85dd9e49c
Milan Matějček <milan.matejcek@csfd.cz>
2016-09-30 09:02:43 +0200

    Seznam upgrade for interval.

--- seznam.cz.js
+++ seznam.cz.js
@@ -1,13 +1,19 @@
-setTimeout(function() {
-    $('div.gadget gadget--firmy').remove();
-    var day = (new Date()).getDay();
-    if (day > 0 && day < 6) {
-        $('div.timeline').remove();
-    } else {
-        $('div.timeline ul.timeline__list li').each(function(k, v) {
-            if($('.timeline-article__header a', v).text() == 'Super') {
-                $(v).remove();
-            }
-        });
-    }
-}, 1000);
+(function(block) {
+    var interval = setInterval(function() {
+        if(!$('div.timeline ul.timeline__list li').size()) {
+            return;
+        }
+        clearInterval(interval);
+
+        var day = (new Date()).getDay();
+        if (day > 0 && day < 6) {
+            $('div.timeline').remove();
+        } else {
+            $('div.timeline ul.timeline__list li').each(function(k, v) {
+                if($('.timeline-article__header a', v).text() == 'Super') {
+                    $(v).remove();
+                }
+            });
+        }
+    }, 500);
+})();
