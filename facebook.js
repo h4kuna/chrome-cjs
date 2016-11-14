610df552ccf9e7f5959066416f768595f1faf0ba
Milan Matějček <milan.matejcek@csfd.cz>
2016-11-14 10:48:11 +0100

    Add facebook.

---
+++ facebook.js
@@ -0,0 +1,10 @@
+(function() {
+	window.addEventListener('load', function() {
+		var date = new Date();
+		var day = date.getDay();
+		var time = date.getHours();
+		if (day > 0 && day < 6 && time > 5 && time < 16) {
+			document.getElementsByTagName('BODY')[0].innerHTML = '<h1>Back to work!!</h1>';
+		}
+	});
+})();
