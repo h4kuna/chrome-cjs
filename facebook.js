8b4907c19170cb8988110e8c6ae5908263028c58
Milan Matějček <milan.matejcek@gmail.com>
2016-11-25 10:49:46 +0100

    Remove handle onload

--- facebook.js
+++ facebook.js
@@ -1,10 +1,8 @@
 (function() {
-	window.addEventListener('load', function() {
-		var date = new Date();
-		var day = date.getDay();
-		var time = date.getHours();
-		if (day > 0 && day < 6 && time > 5 && time < 16) {
-			document.getElementsByTagName('BODY')[0].innerHTML = '<h1>Back to work!!</h1>';
-		}
-	});
+	var date = new Date();
+	var day = date.getDay();
+	var time = date.getHours();
+	if (day > 0 && day < 6 && time > 5 && time < 16) {
+		document.getElementsByTagName('BODY')[0].innerHTML = '<h1>Back to work!!</h1>';
+	}
 })();
