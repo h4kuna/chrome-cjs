72357211606731fe6d51ca62229c431f4809b5f4
Milan Matějček <milan.matejcek@gmail.com>
2017-07-28 15:59:44 +0200

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -4,7 +4,13 @@
     	if (ads.size() === 0) {
     		return;
     	}
-	    ads.remove();
+        ads.remove();
         clearInterval(interval);
     }, 1000);
+    
+    setInterval(function() {
+        $('div[id^=hyperfeed_story_id_]').filter(function(i, element) {
+            return $('div._14bf a._3e_2', element).size() > 0;
+        }).remove();
+    }, 2000);
 })();
