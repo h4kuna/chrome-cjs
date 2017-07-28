68b6b68ff4ba96f626aacd8e4377b7193d535dea
Milan Matějček <milan.matejcek@gmail.com>
2017-07-28 15:31:43 +0200

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -1,9 +1,10 @@
 (function() {
-	var date = new Date();
-	var day = date.getDay();
-	var time = date.getHours();
-	if (day > 0 && day < 6 && time > 5 && time < 16) {
-		document.getElementsByTagName('BODY')[0].innerHTML = '<h1>Back to work!!</h1>';
-	}
-	jQuery('.ego_column').remove();
+    var interval = setInterval(function() {
+    	var ads = jQuery('.ego_column');
+    	if (ads.size() === 0) {
+    		return;
+    	}
+	    ads.remove();
+        clearInterval(interval);
+    }, 1000);
 })();
